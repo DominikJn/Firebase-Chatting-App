88cfd6d961262ad724bd92ef3eb29d8af99629ff
Dominik Jon <rajler33@gmail.com>
2024-07-23 18:31:15 +0200

    buid: Set up firebase configuration file with authentication and firestore

---
+++ firebase-config.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDiFvua1yRxgkz6hGawVjMI4xz67QFUhnI",
+  authDomain: "fir-chatting-app-1aedd.firebaseapp.com",
+  projectId: "fir-chatting-app-1aedd",
+  storageBucket: "fir-chatting-app-1aedd.appspot.com",
+  messagingSenderId: "500281000389",
+  appId: "1:500281000389:web:1a679ad249a4ec8943cbac",
+  measurementId: "G-XRW6P9CTRR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+const analytics = getAnalytics(app);
