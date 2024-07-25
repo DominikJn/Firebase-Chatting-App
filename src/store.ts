2f8d2f62fbdffbd6eb8ca818187bb1ff146dbe76
Dominik Jon <rajler33@gmail.com>
2024-07-25 09:43:34 +0200

    feat: add redux slice for handling friend array

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './features/userSlice'
 import invitesReducer from './features/invitesSlice'
+import friendsReducer from './features/friendsSlice'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    invites: invitesReducer
+    invites: invitesReducer,
+    friends: friendsReducer
   },
 });
 
