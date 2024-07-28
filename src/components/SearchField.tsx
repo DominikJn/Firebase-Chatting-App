24aefda110a635cb43e09270b682b64cbc974150
Dominik Jon <rajler33@gmail.com>
2024-07-28 19:38:30 +0200

    feat: check if searched user is in friends
    Mnow when user is searching another user checkIfFriends
    function loops through the friends array and sets isFriend state
    correctly to the result

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type UserData from "../types/UserData";
 import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import { db } from "../firebase-config";
+import { LiaUserFriendsSolid } from "react-icons/lia";
 
 interface SearchFieldProps {
   searchedUser: UserData;
@@ -11,6 +12,23 @@ interface SearchFieldProps {
 
 const SearchField: React.FC<SearchFieldProps> = ({ searchedUser }) => {
   const user = useSelector((state: RootState) => state.user.value);
+  const friends = useSelector((state: RootState) => state.friends.value);
+  const [isFriend, setFriend] = useState<boolean>(false);
+
+  useEffect(() => {
+    setFriend(checkIfFriends());
+  }, []);
+
+  function checkIfFriends(): boolean {
+    //prevent user from inviting himself
+    if (searchedUser.id === user.uid) return true;
+
+    for (let friend of friends) {
+      if (searchedUser.id === friend.id) return true;
+    }
+
+    return false;
+  }
 
   async function sendInvite(searchedUser: UserData): Promise<void> {
     const userRef = doc(db, "users", searchedUser.id);
@@ -22,9 +40,13 @@ const SearchField: React.FC<SearchFieldProps> = ({ searchedUser }) => {
   return (
     <div className="flex justify-between border-solid border-b">
       <div>{searchedUser.name}</div>
-      <button onClick={() => sendInvite(searchedUser)} className="text-3xl">
-        +
-      </button>
+      {isFriend ? (
+        <LiaUserFriendsSolid />
+      ) : (
+        <button onClick={() => sendInvite(searchedUser)} className="text-3xl">
+          +
+        </button>
+      )}
     </div>
   );
 };
