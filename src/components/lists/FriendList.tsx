65f757fd4ff237db5e1c928e0c96b03c82f155f6
Dominik Jon <rajler33@gmail.com>
2024-07-29 19:24:41 +0200

    feat: add deleting from friends funcionality

--- src/components/lists/FriendList.tsx
+++ src/components/lists/FriendList.tsx
@@ -1,19 +1,36 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
+import type UserData from "../../types/UserData";
+import { arrayRemove, doc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase-config";
 
 const FriendList: React.FC = () => {
+  const user = useSelector((state: RootState) => state.user.value);
   const friends = useSelector((state: RootState) => state.friends.value);
+
+  async function handleFriendDelete(friend: UserData): Promise<void> {
+    //update current users's profile
+    await updateDoc(doc(db, "users", user.uid), {
+      friends: arrayRemove(friend),
+    });
+    //update deleted friend's profile
+    const userAdjustedData = { name: user.name, id: user.uid };
+    await updateDoc(doc(db, "users", friend.id), {
+      friends: arrayRemove(userAdjustedData),
+    });
+  }
 
   return (
     <>
       <h2 className="text-center text-2xl">Friends</h2>
-      {friends.map((invite, index) => (
+      {friends.map((friend: UserData, index: number) => (
         <div
-          key={`${invite.id}-${index}`}
+          key={`${friend.id}-${index}`}
           className="bg-slate-600 m-2 py-2 px-4 rounded-full flex justify-between"
         >
-          <span>{invite.name}</span>
+          <span>{friend.name}</span>
+          <button onClick={() => handleFriendDelete(friend)}>x</button>
         </div>
       ))}
     </>
